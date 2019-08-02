e5dfccac5f5895af0a5d164c5e1908f263346861
Julianna Tetreault <juliannatetreault@gmail.com>
2019-08-02 11:01:38 -0600

    Scope trackers to current store [ch6306]
    Scope trackers to the current store so that browsers will only see
    trackers for the appropriate store.

--- app/controllers/spree/admin/trackers_controller.rb
+++ app/controllers/spree/admin/trackers_controller.rb
@@ -1,2 +1,36 @@
 class Spree::Admin::TrackersController < Spree::Admin::ResourceController
+    before_action :find_tracker, only: [:edit, :update, :destroy]
+
+    def index
+      @trackers = Spree::Tracker.where(store_id: current_store.id)
+    end
+
+    def show
+      @trackers = Spree::Tracker.where(store_id: current_store.id)
+    end
+
+    def new
+      @tracker = Spree::Tracker.new
+    end
+
+    def create
+      @tracker = Spree::Tracker.new(tracker_params)
+
+      if @tracker.save
+        flash[:success] = "Tracker created"
+        redirect_to admin_trackers_path
+      else
+        render :new
+      end
+    end
+
+  private
+
+    def tracker_params
+      params.require(:tracker).permit(:id, :analytics_id, :active, :store_id, :tracker_type, :name, :script, :options, :app_id)
+    end
+
+    def find_tracker
+      @tracker = Spree::Tracker.where(store_id: current_store.id).find(params[:id])
+    end
 end
